c653ab9e08ca48adc74285772bcaa65a0ed4e4a6
Jakub Gawrylkowicz <jgawrylkowicz@gmail.com>
2017-11-30 18:31:21 +0100

    add TTS and pocketsphinx and update .gitignore

--- main.py
+++ main.py
@@ -1,42 +1,77 @@
 import speech_recognition as sr
+from gtts import gTTS
+import os
+import time
 
-WIT_AI_KEY = "ETJDE6YJR44VJT2X4OGDYOLQGGVIWE65"
 trigger = "hey"
 
 # saving the last queries / commands for later use
 # new_dict = dict()
 
 
-# TODO use pocketsphinx for offline recognition
-# capture the voice command and recognize it via wit.ai
-def recognize(rec_audio):
-    try:
-        return r.recognize_wit(rec_audio, key=WIT_AI_KEY)
-    except sr.UnknownValueError:
-        print("Wit.ai could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Wit.ai service; {0}".format(e))
+# recognize the captured audio it via wit.ai (online)
+def recognize_wit(recognizer, audio):
+    if audio is not None:
+        wit_ai_key = "ETJDE6YJR44VJT2X4OGDYOLQGGVIWE65"
+        try:
+            return recognizer.recognize_wit(audio, key=wit_ai_key)
+        except sr.UnknownValueError:
+            print("Wit.ai could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Wit.ai service; {0}".format(e))
+    else:
+        say("Sorry, I didn't catch that")
+
+
+# recognize the captured audio it via sphinx (offline)
+def recognize_sphinx(recognizer, audio):
+    if audio is not None:
+        try:
+            return recognizer.recognize_sphinx(audio, language="en-US")
+        except sr.UnknownValueError:
+            print("Sphinx could not understand audio")
+        except sr.RequestError as e:
+            print("Sphinx error; {0}".format(e))
+
+
+def say(text):
+    if text is not None:
+        tts = gTTS(text=text, lang="en")
+        tts.save("response.mp3")
+        # mpg321 for linux / pi
+        os.system("afplay response.mp3")
+        print("HeyPi: " + text)
 
 
 # listen to voice commands
-def listen(audio_source):
-    while 1:
-        audio = r.listen(audio_source)
-        #command = recognize(audio)
-        #print(command)
+def listen_from_source(recognizer, audio_source):
+    # TODO use background_listening for the commands and the main thread for the catchphrase
 
-        if r.recognize_wit(audio, key=WIT_AI_KEY) == trigger:
+    #while 1:
+        print("Waiting for catchphrase")
+
+        rec_audio = recognizer.listen(audio_source)
+        command = recognize_sphinx(recognizer, rec_audio)
+        print("You: " + command)
+
+        if trigger in command:
             # Trigger recognized, listening to the command
-            recognize(audio)
-        else:
-            # Voice captured, but the trigger wasn't recognized
-            print("Trigger not recognized")
+            say("I'm listening")
+
+            rec_audio = recognizer.listen(audio_source)
+            command = recognize_wit(recognizer, rec_audio)
+
+            print("You: " + command)
 
 
-r = sr.Recognizer()
-with sr.Microphone() as source:
-    print("Say something!")
-    listen(source)
+def init():
+    rec = sr.Recognizer()
+    mic = sr.Microphone()
+    with mic as source:
+        rec.adjust_for_ambient_noise(source)
+        rec.pause_threshold = 0.8
+        listen_from_source(rec, source)
 
 
+init()
 
